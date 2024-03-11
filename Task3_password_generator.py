e95137404a37a37a077b7c02a27a16756b1abfb3
Suhail Khan <159753384+suhxill@users.noreply.github.com>
2024-03-11 20:04:41 +0530

    Update Task3_password_generator.py

--- Task3_password_generator.py
+++ Task3_password_generator.py
@@ -11,23 +11,17 @@ def generate_password(length):
     A random password string.
   """
 
-
-  # Define the characters that can be used in the password.
   characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
 
-  # Generate a random password.
   password = ""
   for i in range(length):
     password += random.choice(characters)
 
-  # Return the password.
   return password
 
-# Get the desired password length from the user.
 length = int(input("Enter the desired password length: "))
 
-# Generate a random password.
 password = generate_password(length)
 
-# Print the password.
+
 print("Your random password is:", password)
